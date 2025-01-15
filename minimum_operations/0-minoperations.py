d0da88d4a316c87eb1eebabae61e210c5ad96e72
Yvantrey <rugambayvan1@gmail.com>
2025-01-15 13:26:52 +0000

    0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
 """Minimum Operations"""
+import math
 
 
 def minOperations(n):
-    """Calculation  of operations needed to obtain exactly n H characters
-
-    Args:
-        n (int): The target number of H characters.
-
-    Returns:
-        int: The minimum number of operations required.
-    """
-
+    """Minimum Operations"""
     if n <= 1:
         return 0
 
     operations = 0
-    i = 2
-    while i <= n:
-        if n % i == 0:
+    for i in range(2, int(math.sqrt(abs(n))) + 1):
+        while n % i == 0:
             operations += i
             n //= i
-        else:
-            i += 1
+
+    if n > 1:
+        operations += n
 
     return operations
 ~
